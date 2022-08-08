06f373a57731d570f524adc50271775346388e19
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-07 20:48:19 -0400

    CodeFirst - DbContext created.

--- CodeFirstDemo/Program.cs
+++ CodeFirstDemo/Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CodeFirstDemo
 {
+    public class Post
+    {
+        public int Id { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+    
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+    
     internal class Program
     {
         static void Main(string[] args)
