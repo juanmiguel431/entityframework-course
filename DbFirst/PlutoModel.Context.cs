b6bc95c082d920551af8a5092c98b760d4cbe7fd
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-08 20:18:36 -0400

    Handling changes into the database.

--- DbFirst/PlutoModel.Context.cs
+++ DbFirst/PlutoModel.Context.cs
@@ -31,5 +31,6 @@ namespace DbFirst
         public virtual DbSet<Post> Posts { get; set; }
         public virtual DbSet<Tag> Tags { get; set; }
         public virtual DbSet<tblUser> tblUsers { get; set; }
+        public virtual DbSet<UserProfile> UserProfiles { get; set; }
     }
 }
