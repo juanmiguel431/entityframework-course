58476ca38a7ba102c9aea5579d15e5ee65e0a796
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-09 22:42:14 -0400

    DeleteDatePublishedColumnFromCoursesTable migration added.

--- CodeFirstExistingDatabase/Course.cs
+++ CodeFirstExistingDatabase/Course.cs
@@ -23,8 +23,6 @@ namespace CodeFirstExistingDatabase
 
         public Category Category { get; set; }
 
-        public DateTime? DatePublished { get; set; }
-
         public int Level { get; set; }
 
         public float FullPrice { get; set; }
