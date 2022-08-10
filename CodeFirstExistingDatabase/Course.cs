6c000dc20f756c877e8c591963c7042f7712ba15
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-09 22:33:03 -0400

    RenameTitleToNameInCoursesTable migration added.

--- CodeFirstExistingDatabase/Course.cs
+++ CodeFirstExistingDatabase/Course.cs
@@ -17,7 +17,7 @@ namespace CodeFirstExistingDatabase
 
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
 
---
+++ CodeFirstExistingDatabase/Migrations/202208100226177_RenameTitleToNameInCoursesTable.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeFirstExistingDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameTitleToNameInCoursesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "Name", c => c.String(nullable: false));
+            Sql("Update dbo.Courses SET Name = Title");
+            DropColumn("dbo.Courses", "Title");
+            
+            // RenameColumn("dbo.Courses", "Title", "Name");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Courses", "Title", c => c.String(nullable: false));
+            Sql("Update dbo.Courses SET Title = Name");
+            DropColumn("dbo.Courses", "Name");
+            
+            // RenameColumn("dbo.Courses", "Name", "Title");
+        }
+    }
+}
