6cde2933ccd8401d667ed621cc0bd323ec640508
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-10 20:02:46 -0400

    VidzyExercise.csproj - Migrations enabled.

---
+++ VidzyExercise/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace VidzyExercise.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VidzyExercise.Models.VidzyExerciseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VidzyExercise.Models.VidzyExerciseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
