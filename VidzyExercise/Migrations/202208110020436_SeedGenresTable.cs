7f224406d47b456e2070c8c9e795ebc903c3f324
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-10 20:22:26 -0400

    SeedGenresTable migration added.

---
+++ VidzyExercise/Migrations/202208110020436_SeedGenresTable.cs
@@ -0,0 +1,23 @@
+﻿namespace VidzyExercise.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedGenresTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"INSERT INTO dbo.Genres (Id, Name) VALUES (1, 'Comedy');
+                INSERT INTO dbo.Genres (Id, Name) VALUES (2, 'Action');
+                INSERT INTO dbo.Genres (Id, Name) VALUES (3, 'Horror');
+                INSERT INTO dbo.Genres (Id, Name) VALUES (4, 'Thriller');
+                INSERT INTO dbo.Genres (Id, Name) VALUES (5, 'Family');
+                INSERT INTO dbo.Genres (Id, Name) VALUES (6, 'Romance');");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM dbo.Genres WHERE Id IN (1, 2, 3, 4, 5, 6)");
+        }
+    }
+}
