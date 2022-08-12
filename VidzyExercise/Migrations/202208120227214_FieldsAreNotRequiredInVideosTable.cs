dd869af0b8e0f24638d7c57673d8ba7d8303c0fa
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-11 22:31:31 -0400

    FieldsAreNotRequiredInVideosTable migration added.

---
+++ VidzyExercise/Migrations/202208120227214_FieldsAreNotRequiredInVideosTable.cs
@@ -0,0 +1,28 @@
+﻿namespace VidzyExercise.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FieldsAreNotRequiredInVideosTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Videos", "Genre_Id", "dbo.Genres");
+            DropIndex("dbo.Videos", new[] { "Genre_Id" });
+            AlterColumn("dbo.Videos", "Name", c => c.String());
+            AlterColumn("dbo.Videos", "Genre_Id", c => c.Byte());
+            CreateIndex("dbo.Videos", "Genre_Id");
+            AddForeignKey("dbo.Videos", "Genre_Id", "dbo.Genres", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Videos", "Genre_Id", "dbo.Genres");
+            DropIndex("dbo.Videos", new[] { "Genre_Id" });
+            AlterColumn("dbo.Videos", "Genre_Id", c => c.Byte(nullable: false));
+            AlterColumn("dbo.Videos", "Name", c => c.String(nullable: false));
+            CreateIndex("dbo.Videos", "Genre_Id");
+            AddForeignKey("dbo.Videos", "Genre_Id", "dbo.Genres", "Id", cascadeDelete: true);
+        }
+    }
+}
--- VidzyExercise/Models/Video.cs
+++ VidzyExercise/Models/Video.cs
@@ -7,15 +7,9 @@ namespace VidzyExercise.Models
     public class Video
     {
         public int Id { get; set; }
-        
-        [Required]
         public string Name { get; set; }
-        
         public DateTime ReleaseDate { get; set; }
-        
-        [Required]
         public Genre Genre { get; set; }
-
         public Classification Classification { get; set; }
     }
 }
